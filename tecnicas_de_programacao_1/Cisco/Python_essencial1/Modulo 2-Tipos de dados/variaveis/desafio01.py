3bfdca92248add343fe8873f0f7acdac77d5aaed
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-26 21:20:01 -0300

    atualizar

---
+++ tecnicas_de_programacao_1/Cisco/Python_essencial1/Modulo 2-Tipos de dados/variaveis/desafio01.py
@@ -0,0 +1,23 @@
+john = 5
+mary = 14
+adam = 2
+print(f"John:{john}, Mary: {mary}, Adam: {adam}") 
+total_apples = john + mary + adam
+print(f"Total maças: {total_apples}")
+
+john_mary = john + mary
+mary_adam = mary+adam
+adam_john = adam + john
+print(john_mary)
+print(mary_adam)
+print(adam_john)
+
+
+multiplica = adam * john * mary
+exponenciacao = adam ** john ** mary
+divisao = mary / adam // john
+modulo = mary % adam % john
+print(multiplica)
+print(exponenciacao)
+print(divisao)
+print(modulo)
