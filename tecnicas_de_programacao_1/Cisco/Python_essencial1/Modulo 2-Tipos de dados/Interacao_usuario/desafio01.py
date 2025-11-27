d531bb97b758d1c1a7c7844b16e5b4a7c0ac5b5d
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-26 21:36:21 -0300

    atualização

---
+++ tecnicas_de_programacao_1/Cisco/Python_essencial1/Modulo 2-Tipos de dados/Interacao_usuario/desafio01.py
@@ -0,0 +1,20 @@
+# entre com um valor float para a variável a aqui
+# entre com um valor float para a variável b aqui
+
+# imprima o resultado da adição aqui
+# imprima o resultado da subtração aqui
+# imprima o resultado da multiplicação aqui
+# imprima o resultado da divisão aqui
+
+var_a = float(input("Digite um numero em R$(exemplo: 2,1): "))
+var_b = float(input("Digite outro numero em R$(exemplo: 2,1): "))
+soma = var_a + var_b
+print(soma)
+subtracao = var_a - var_b
+print(subtracao)
+multiplicacao = var_a * var_b
+print(multiplicacao)
+divisao = var_a / var_b
+print(divisao)
+
+
