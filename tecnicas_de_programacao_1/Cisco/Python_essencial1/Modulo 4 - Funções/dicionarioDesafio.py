2d7547d7cb468f518337a9456e50b2dca74e1ab0
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-27 18:13:55 -0300

    atualização

---
+++ tecnicas_de_programacao_1/Cisco/Python_essencial1/Modulo 4 - Funções/dicionarioDesafio.py
@@ -0,0 +1,10 @@
+precos = {"celular": 1200, "computador": 5000, "tablet": 2000}
+print("Items: ")
+for i in precos.items():
+    print(i)
+print("Chaves: ")
+for i in precos.keys():
+    print(i)
+print("Valores: ")
+for i in precos.values():
+    print(i)
