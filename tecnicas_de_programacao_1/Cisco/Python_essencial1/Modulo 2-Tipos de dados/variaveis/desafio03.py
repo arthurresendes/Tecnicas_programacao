69315a1b14cad1019993ee7e3624c1f22c2a36b6
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-26 21:28:32 -0300

    atualização

---
+++ tecnicas_de_programacao_1/Cisco/Python_essencial1/Modulo 2-Tipos de dados/variaveis/desafio03.py
@@ -0,0 +1,9 @@
+# 3x3 - 2x2 + 3x - 1
+
+cont = 0
+while cont != 3:
+    x = int(input("Digite um numero inteiro: "))
+    y = 3 * x ** 3 - 2 *x ** 2 + 3 * x - 1
+    print(float(y))
+    cont += 1
+
