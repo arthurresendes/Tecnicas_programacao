34302381219dc97f62a6402eef6115076f7e6af5
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-02 20:07:01 -0300

    atualizar

---
+++ tecnicas_de_programacao_1/Cisco/Python_essencial1/Modulo 3/Decisao/arq002.py
@@ -0,0 +1,6 @@
+
+var = input("Digite Spathiphyllum: ")
+if var == "Spathiphyllum":
+    print("Spathiphyllum é a melhor fábrica de todos os tempos!")
+else:
+    print("Não, eu quero um grande Spathiphyllum!")
