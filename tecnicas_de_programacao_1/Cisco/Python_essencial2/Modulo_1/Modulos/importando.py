4498aa5a40ca20b403f8a7dd2dcccb0cc4db2c5f
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-02 19:22:17 -0300

    atualoizar

--- tecnicas_de_programacao_1/Cisco/Python_essencial2/Modulo_1/Modulos/importando.py
+++ tecnicas_de_programacao_1/Cisco/Python_essencial2/Modulo_1/Modulos/importando.py
@@ -1,3 +1,11 @@
 import math
 
 print(math.factorial(5))
+
+import time, pandas as pd
+time.sleep(5)
+
+dataframe = {"Numeros": [1,2,3,4,5,6,7,8,9,0], "Letras": ["A", "B", "C", "D", "E", "F", "G", "H", "J", "I"]}
+
+df = pd.DataFrame(dataframe, index=False)
+print(df)
