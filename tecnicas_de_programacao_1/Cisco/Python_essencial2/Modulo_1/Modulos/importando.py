0e94d69465a280fb8f4b8f7533d76f92ad68ad7c
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-02 19:24:36 -0300

    atualização

--- tecnicas_de_programacao_1/Cisco/Python_essencial2/Modulo_1/Modulos/importando.py
+++ tecnicas_de_programacao_1/Cisco/Python_essencial2/Modulo_1/Modulos/importando.py
@@ -7,5 +7,5 @@ time.sleep(5)
 
 dataframe = {"Numeros": [1,2,3,4,5,6,7,8,9,0], "Letras": ["A", "B", "C", "D", "E", "F", "G", "H", "J", "I"]}
 
-df = pd.DataFrame(dataframe, index=False)
+df = pd.DataFrame(dataframe)
 print(df)
