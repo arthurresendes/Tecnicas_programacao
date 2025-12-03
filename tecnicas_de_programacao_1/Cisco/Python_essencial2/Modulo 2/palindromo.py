2e0b6cf65e68ed69c87d50ab89645004676d2663
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-03 19:18:32 -0300

    atualização

---
+++ tecnicas_de_programacao_1/Cisco/Python_essencial2/Modulo 2/palindromo.py
@@ -0,0 +1,12 @@
+
+
+def palindormo(palavra: str) -> str:
+    palavra = palavra.lower()
+    palavra_invertida = palavra[::-1]
+    print(palavra_invertida)
+    if palavra_invertida == palavra:
+        return True
+    else:
+        return False
+
+print(palindormo("Roma é amor"))
