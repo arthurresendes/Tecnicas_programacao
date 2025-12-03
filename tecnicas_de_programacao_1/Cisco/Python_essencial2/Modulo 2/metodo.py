ee0aa4f9baf246937d3386fe9cc39c676bf02adf
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-03 18:23:28 -0300

    atualizar

---
+++ tecnicas_de_programacao_1/Cisco/Python_essencial2/Modulo 2/metodo.py
@@ -0,0 +1,49 @@
+word = 'Ola Mundo'
+
+
+print(word.capitalize())
+print('[' + word.center(20) + ']')
+
+# Verificva se termina com certa sequencia e retorna true or false
+
+print(word.endswith('al'))
+if word.endswith('do'):
+    print("Termina com do")
+else:
+    print("Nao")
+
+print(word.find('ndo'))
+print(word.find('a'))
+
+# Verifica se tem digito e numeros ao mesmo tempo
+print(word.isalnum())
+print('Arthur01'.isalnum())
+
+# Verifica se tem apenas letras
+print("Oi".isalpha())
+print(word.isalpha())
+print('Arthur01'.isalpha())
+
+# Verifica se tem apenas digitos
+print("011006".isdigit())
+print(word.isdigit())
+print('Arthur01'.isdigit())
+
+# Verifica se é tudo minuscula
+print("Oi".islower())
+print(word.islower())
+print('arthur01'.islower())
+
+# Verifica se tudo é maiuscula
+print("OI".isupper())
+print(word.isupper())
+print('arthur01'.isupper())
+
+# Verifica espacos 
+print(" ".isspace())
+print(word.isspace())
+print('arthur01'.isspace())
+
+# Join
+print(",".join([word] + ["Arthur"]))
+
