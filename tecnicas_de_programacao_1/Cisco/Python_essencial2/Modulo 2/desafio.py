9a62943874a327164533bac3101f4bf36371556d
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-03 18:48:20 -0300

    atualizar

---
+++ tecnicas_de_programacao_1/Cisco/Python_essencial2/Modulo 2/desafio.py
@@ -0,0 +1,17 @@
+
+
+def mysplit(palavra: str) -> list[str]:
+    palavra_final = []
+    palavra_atual = ""
+    for c in palavra:
+        if c != ' ':
+            palavra_atual += c
+        else:
+            if palavra_atual:
+                palavra_final.append(palavra_atual)
+                palavra_atual = "" # Zera palavra
+    if palavra_atual:
+        palavra_final.append(palavra_atual)
+    return palavra_final
+
+print(mysplit("O mundo tem que ser mais feliz"))
