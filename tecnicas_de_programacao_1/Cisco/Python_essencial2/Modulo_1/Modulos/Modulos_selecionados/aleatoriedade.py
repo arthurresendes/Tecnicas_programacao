6374325b40aaa4a81454b5b495ac41abd74eeb70
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-04 19:31:14 -0300

    atualizar

--- tecnicas_de_programacao_1/Cisco/Python_essencial2/Modulo_1/Modulos/Modulos_selecionados/aleatoriedade.py
+++ tecnicas_de_programacao_1/Cisco/Python_essencial2/Modulo_1/Modulos/Modulos_selecionados/aleatoriedade.py
@@ -7,3 +7,11 @@ for i in range(5):
 lista = [1,2,3,4,5,6,7,8,9,10]
 print(choice(lista))
 print(sample(lista,5))
+
+import random
+random.seed(42)
+first_random_number = random.randint(1, 100)
+print(f"First random number with seed 42: {first_random_number}")
+random.seed(100)
+second_random_number = random.randint(1, 100)
+print(f"Second random number with seed 100: {second_random_number}")
