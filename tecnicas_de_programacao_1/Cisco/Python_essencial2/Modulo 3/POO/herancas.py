88057d6fa4af04918c0553873652bfc3af5a7f28
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-09 20:57:16 -0300

    atualizar

--- tecnicas_de_programacao_1/Cisco/Python_essencial2/Modulo 3/POO/herancas.py
+++ tecnicas_de_programacao_1/Cisco/Python_essencial2/Modulo 3/POO/herancas.py
@@ -47,3 +47,81 @@ print(obj.subVar)
 print(obj.supVar)
 
 
+
+class Level1:
+    variable_1 = 100
+    def __init__(self):
+        self.var_1 = 101
+
+    def fun_1(self):
+        return 102
+
+
+class Level2(Level1):
+    variable_2 = 200
+    def __init__(self):
+        super().__init__()
+        self.var_2 = 201
+    
+    def fun_2(self):
+        return 202
+
+
+class Level3(Level2):
+    variable_3 = 300
+    def __init__(self):
+        super().__init__()
+        self.var_3 = 301
+
+    def fun_3(self):
+        return 302
+
+
+obj = Level3()
+
+print(obj.variable_1, obj.var_1, obj.fun_1())
+print(obj.variable_2, obj.var_2, obj.fun_2())
+print(obj.variable_3, obj.var_3, obj.fun_3())
+
+class Level1:
+    var = 100
+    def fun(self):
+        return 101
+
+
+class Level2(Level1):
+    var = 200
+    def fun(self):
+        return 201
+
+
+class Level3(Level2):
+    pass
+
+
+obj = Level3()
+
+print(obj.var, obj.fun())
+
+class Left:
+    var = "L"
+    var_left = "LL"
+    def fun(self):
+        return "Left"
+
+
+class Right:
+    var = "R"
+    var_right = "RR"
+    def fun(self):
+        return "Right"
+
+
+class Sub(Left, Right):
+    pass
+
+
+obj = Sub()
+
+print(obj.var, obj.var_left, obj.var_right, obj.fun())
+
